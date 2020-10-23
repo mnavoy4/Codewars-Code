2a2ce9520f0650ed24003f78f51a58c07589b750
mnavoy4 <mnavoy4@gmail.com>
2020-10-23 14:45:05 -0600

    10/23/2020 solution

--- Javascript/CodeWars.js
+++ Javascript/CodeWars.js
@@ -60,3 +60,19 @@ function getDivisorsCnt(n){
   }
   return count
 }
+
+// ATM machines allow 4 or 6 digit PIN codes and PIN codes cannot contain anything but exactly 4 digits or exactly 6 digits.
+// If the function is passed a valid PIN string, return true, else return false.
+
+function validatePIN (pin) {
+  let isnum = /^\d+$/.test(pin);
+  if(pin.length === 4 || pin.length === 6){
+    if (isnum) {
+      return true;
+    } else {
+      return false;
+    }
+  } else {
+    return false;
+  }
+}
