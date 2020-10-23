82d32206ed08f01fa2031f9fa802c33e673dcfef
mnavoy4 <mnavoy4@gmail.com>
2020-10-23 09:01:33 -0600

    10/23/2020 codewars solution

--- Javascript/CodeWars.js
+++ Javascript/CodeWars.js
@@ -10,3 +10,29 @@ function removeSmallest(numbers) {
   const position = numbers.indexOf(smallestNum);
   return numbers.slice(0, position).concat(numbers.slice(position + 1))
 }
+
+// You live in the city of Cartesia where all roads are laid out in a perfect grid.
+// You arrived ten minutes too early to an appointment, so you decided to take the opportunity to go for a short walk.
+// The city provides its citizens with a Walk Generating App on their phones -- everytime you press the button it sends you an array of one-letter strings representing directions to walk (eg. ['n', 's', 'w', 'e']). You always walk only a single block for each letter (direction) and you know it takes you one minute to traverse one city block, so create a function that will return true if the walk the app gives you will take you exactly ten minutes (you don't want to be early or late!) and will, of course, return you to your starting point.
+// Return false otherwise.
+
+function isValidWalk(walk) {
+  let vertical = 0
+  let horizontal = 0
+  walk.forEach(function(element){
+    if(element === 'n'){
+      vertical++;
+    } else if(element === 's'){
+      vertical--;
+    } else if(element === 'e'){
+      horizontal++;
+    } else {
+      horizontal--;
+    }
+  })
+  if(vertical == 0 && horizontal == 0 && walk.length == 10){
+    return true;
+  } else {
+    return false;
+  }
+}
