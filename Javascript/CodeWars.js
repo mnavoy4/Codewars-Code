96d9ea64d0e56f3370835c3e233e7a3a5a46e121
mnavoy4 <mnavoy4@gmail.com>
2020-10-23 09:46:15 -0600

    10/23/2020

--- Javascript/CodeWars.js
+++ Javascript/CodeWars.js
@@ -36,3 +36,14 @@ function isValidWalk(walk) {
     return false;
   }
 }
+
+// Bob is preparing to pass IQ test. The most frequent task in this test is to find out which one of the given numbers
+// differs from the others. Bob observed that one number usually differs from the others in evenness.
+// Help Bob — to check his answers,he needs a program that among the given numbers finds one that is different in evenness, and return a position of this number.
+
+function iqTest(numbers){
+  let arrayOfNumbers = numbers.split(" ").map(x => x % 2);
+  let sum = arrayOfNumbers.reduce((a,b) => a + b);
+  let targetIndex = sum > 1 ? 0 : 1;
+  return arrayOfNumbers.indexOf(targetIndex) + 1;
+}
