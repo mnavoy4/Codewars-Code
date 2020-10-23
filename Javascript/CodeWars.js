5f930d58d736c89bc27e90b86ed714b75a99f614
mnavoy4 <mnavoy4@gmail.com>
2020-10-23 14:37:34 -0600

    another 10/23/2020 codewars

--- Javascript/CodeWars.js
+++ Javascript/CodeWars.js
@@ -47,3 +47,16 @@ function iqTest(numbers){
   let targetIndex = sum > 1 ? 0 : 1;
   return arrayOfNumbers.indexOf(targetIndex) + 1;
 }
+
+// Count the number of divisors of a positive integer n.
+// Random tests go up to n = 500000.
+
+function getDivisorsCnt(n){
+  let count = 0;
+  for (let i = 1; i <= n; i++){
+    if (n % i === 0){
+      count++;
+    }
+  }
+  return count
+}
