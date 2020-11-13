d02e7d7f0b82f43a3d12124fc4f6988c973f18c5
mnavoy4 <mnavoy4@gmail.com>
2020-11-13 07:40:39 -0700

    11/13/2020 code wars

--- Javascript/CodeWars.js
+++ Javascript/CodeWars.js
@@ -121,3 +121,27 @@ function deleteNth(arr,n){
   })
   return returnedArray;
 }
+
+// Let us begin with an example:
+// A man has a rather old car being worth $2000. He saw a secondhand car being worth $8000. He wants to keep his old car until he can buy the secondhand one.
+// He thinks he can save $1000 each month but the prices of his old car and of the new one decrease of 1.5 percent per month. Furthermore this percent of loss increases of 0.5 percent at the end of every two months. Our man finds it difficult to make all these calculations.
+// Can you help him?
+// How many months will it take him to save up enough money to buy the car he wants, and how much money will he have left over?
+
+function nbMonths(startPriceOld, startPriceNew, savingperMonth, percentLossByMonth){
+  //your code here
+  if (startPriceNew <= startPriceOld){
+    return [0, Math.round(startPriceOld - startPriceNew)];
+  };
+  let months = 0;
+  let total = startPriceOld;
+  while (startPriceNew >= total){
+    months++;
+    total += savingperMonth;
+    total -= startPriceOld * percentLossByMonth / 100
+    startPriceNew -= startPriceNew * percentLossByMonth / 100;
+    startPriceOld -= startPriceOld * percentLossByMonth / 100;
+    months % 2 ? percentLossByMonth += 0.5 : percentLossByMonth;
+  }
+  return [months, Math.round(total - startPriceNew)]
+}
